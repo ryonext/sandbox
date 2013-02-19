920e51d1f8f26684a1ace299f0abb81ff61dc830
ryonext <ryonext.s@gmail.com>
2013-02-19 22:07:16 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAncestry::Application.config.secret_token = 'd48dbc4f12e1446f49920a9f5a69da20b8a652d04b6890b23c00e9355810d09bfff5c3ea4816a7a1ff70eb70f4d6abed5cef104ac0a228e388841b59f06b5c8b'
